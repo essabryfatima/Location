540312b2c23c889819f76c49205ef7e679cbb93e
essabryfatima <essabryfatima22@gmail.com>
2026-02-28 23:52:31 +0000

    Add Tailwind CSS configuration for black and red theme

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        red: {
+          light: '#FF0000',
+          DEFAULT: '#C02020',
+          dark: '#800000',
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
